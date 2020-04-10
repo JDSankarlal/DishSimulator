7a3dc3ac89ac72f3ecf2cd4e70e6a033e7f47190
Joshua Sankarlal <joshua.sankarlal@uoit.net>
2020-04-10 00:58:26 -0400

    Plate click and drag

---
+++ DishSim/Assets/_Scripts/ClickAndDrag.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickAndDrag : MonoBehaviour
+{
+    bool isPicked = false;
+
+    void Update()
+    {
+        if (isPicked)
+        {
+            Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition); 
+            //if MouseOver is true, and MouseButton(0) is true, move plate to mouse position
+            transform.position = (pos);
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            isPicked = false; //If mousebutton released, isPicked = false. Should stop plate tracking mouse pos?
+        }       
+    }
+   
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            isPicked = true;   //Basic shit.
+        }   
+    }
+
+    // void OnMouseExit() //Also self explanatory
+    // {
+    //    isPicked = false;
+    // }
+
+
+}
